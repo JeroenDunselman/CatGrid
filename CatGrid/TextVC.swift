ea2719f8443d4fe73264efb49e896d9645f55b93
Jeroen Dunselman <jeroduns@gmail.com>
2017-03-31 15:35:11 +0200

    TextVC ActivityView protocol

--- CatGrid/TextVC.swift
+++ CatGrid/TextVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TextVC: UIViewController {
+class TextVC: UIViewController, ActivityView {
   @IBOutlet weak var webView: UIWebView!
 
   @IBOutlet weak var activityView: UIActivityIndicatorView!
@@ -20,7 +20,7 @@ class TextVC: UIViewController {
     super.viewDidLoad()
     self.activityView.startAnimating()
     self.webView.isHidden = true
-    self.webView.showLoremScript()
+    self.webView.showLoremScript(vc: self)
   }
   
   override func didReceiveMemoryWarning() {
@@ -29,13 +29,17 @@ class TextVC: UIViewController {
   }
   
   func presentView() {
-  
+    activityView.stopAnimating()
+    webView.isHidden = false
   }
 }
 
+protocol ActivityView : NSObjectProtocol {
+  func presentView()
+}
+
 extension UIWebView {
-  
-  func showLoremScript() {
+  func showLoremScript(vc: ActivityView) {
     let loremUrl:String = "http://loripsum.net/api/10/short/headers"
     
     // Create URL
@@ -53,77 +57,11 @@ extension UIWebView {
       self.loadHTMLString(responseString as! String, baseURL: nil)
       
       DispatchQueue.main.async {
-        self.isHidden = false
+        vc.presentView()
       }
     }
     task.resume()
   }
 }
 
-//      if error != nil
-//      {
-//        print("error=\(error)")
-//        return
-//      }
-//    let scriptUrl = loremUrl //"http://swiftdeveloperblog.com/my-http-get-example-script/"
-//    // Add one parameter
-//    let urlWithParams = loremUrl //scriptUrl + "?userName=\(userNameValue!)"
-//    // Create NSURL Ibject
 
-
-
-//  let strY:String = "<!DOCTYPE html><html><head><title>Hello Lorem Ipsump</title><link rel=\"stylesheet\" href=\"myCSS.css\" type=\"text/css\"></head><body></body>"
-//    let myURL:String = strX
-//      Bundle.main.url(forResource: "myHtml",withExtension: "html")
-
-//    var txtURL: URL!
-//    txtURL = URL(string: strX)
-//    let txtURL:URL = URL(string: strLoremUrl)!
-//    let requestObj = NSURLRequest(url:txtURL)
-//    var readLorem:UIWebView = UIWebView()
-//    readLorem.loadRequest(requestObj as URLRequest)
-////    let strResult:String = readLorem.str
-//    let doc = readLorem.stringByEvaluatingJavaScript(from: "document.documentElement.outerHTML")
-
-
-
-
-//      // Convert server json response to NSDictionary
-//      do {
-//        if let convertedJsonIntoDict = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
-//
-//          // Print out dictionary
-//          print(convertedJsonIntoDict)
-//
-//          // Get value by key
-//          let firstNameValue = convertedJsonIntoDict["userName"] as? String
-//          print(firstNameValue!)
-//
-//        }
-//      } catch let error as NSError {
-//        print(error.localizedDescription)
-//      }
-//  var strLorem:String = ""
-//  func networkFunction(query: String, completion: @escaping (_ success: Bool) -> Void) {
-//    var url = strLoremUrl //"http://notrealurl.com/etix.asp?t=scanticket&sid=100000&tid=" + query
-//
-//    let task = URLSession.shared.dataTask(with: NSURL(string: url)! as URL, completionHandler: { (responseData, response, error) -> Void in
-//      if error == nil {
-//        let feedback = NSString(data: responseData!, encoding: String.Encoding.utf8.rawValue)
-//        strLorem = responseData
-//        completion(feedback == "valid")
-//      } else {
-//        print(error)
-//        completion(false)
-//      }
-//    })
-//  }
-/*
- // MARK: - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
- // Get the new view controller using segue.destinationViewController.
- // Pass the selected object to the new view controller.
- }
- */
